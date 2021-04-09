3a723f186fd54cc5cb1937e784d313a878fffe70
Nurdin <ulanovnurdin@gmail.com>
2021-04-10 02:49:52 +0600

    edit html

--- cafe/resources/views/home.blade.php
+++ cafe/resources/views/home.blade.php
@@ -4,17 +4,14 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+            <div class="content">
+                <div class="title m-b-md">
+                    <center> <h1> А тут ничего нет!!! </h1> </center>
                 </div>
             </div>
         </div>
