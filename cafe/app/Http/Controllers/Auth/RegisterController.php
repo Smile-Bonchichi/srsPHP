801302513e35fa84eac3b5b9d3c8b7d1820ab517
Nurdin <ulanovnurdin@gmail.com>
2021-04-10 01:14:58 +0600

    add registration

--- cafe/app/Http/Controllers/Auth/RegisterController.php
+++ cafe/app/Http/Controllers/Auth/RegisterController.php
@@ -10,17 +10,6 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
     use RegistersUsers;
 
     /**
@@ -66,6 +55,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'role' => $data['role'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- cafe/resources/views/auth/register.blade.php
+++ cafe/resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Тип пользователя') }}</label>
                                 <div class="col-md-6">
-                                    <select name="type1" class="form-control">
+                                    <select name="role" class="form-control">
                                         <option value="Kitchen">Кухня</option>
                                         <option value="Waiter">Официант</option>
                                         <option value="Manager">Менеджер</option>
