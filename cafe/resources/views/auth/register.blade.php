c6c63899220ec37ea2ce4fb1d762023dfecf4e82
Nurdin <ulanovnurdin@gmail.com>
2021-04-10 02:38:40 +0600

    edit regis

--- cafe/resources/views/auth/register.blade.php
+++ cafe/resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Тип пользователя') }}</label>
+                            <label for="type" class="col-md-4 col-form-label text-md-right">{{ __('Тип пользователя') }}</label>
                                 <div class="col-md-6">
                                     <select name="role" class="form-control">
                                         <option value="Kitchen">Кухня</option>
