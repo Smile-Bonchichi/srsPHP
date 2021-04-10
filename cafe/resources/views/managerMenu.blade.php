618c050c0028bc883fc5dad34ae6b6e870597cf6
Bermet <bermetk.6789@gmail.com>
2021-04-10 18:43:59 +0600

    add blade

---
+++ cafe/resources/views/managerMenu.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<a href = "{{ url('/addFood') }}" > Добавить блюдо </a>
+
+@endsection
--- cafe/routes/web.php
+++ cafe/routes/web.php
@@ -4,6 +4,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/managerMenu', function () {
+    return view('managerMenu');
+});
+
+Route::get('/addFood', function () {
+    return view('addFood');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
